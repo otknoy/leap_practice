6f9306b95a19f912d5b8d569b5e9fe8b7d1696a2
asdmay <wrbmc.mc.pr@gmail.com>
2015-08-28 15:36:59 +0900

    leapを使っているやつは、leapMotion.jsになった

--- js/leapMotion.js
+++ js/leapMotion.js
@@ -32,4 +32,25 @@ Leap.loop({enableGestures: true}, function(frame){
 
     var hand = frame.hands[0];
     var index = hand.indexFinger;
-    var point = get.
+    var point = getFingertip(finger);
+    points.push(point);
+});
+
+function getFingertip(finger){
+    var point = {"x": finger.tipPosition[0],
+		 "y": finger.tipPosition[1],
+		 "z": finger.tipPosition[2]
+		};
+    return point;
+}
+
+function keyTapped(frame){
+    var gestures = frame.gestures;
+    for (var i = 0; i < gestures.length; i++){
+	if(gestures[i].type == "keyTap"){
+	    return true;
+	}
+    }
+    return false;
+}
+    
--- js/sketch.js
+++ js/sketch.js
@@ -1,66 +1,28 @@
-
-
 function sketchProc(processing) {
+    var count;
     processing.setup = function(){
 	processing.size(600, 600, processing.P3D);
-	processing.background(0);
 	processing.fill(processing.color(255, 0, 255));
 	processing.stroke(processing.color(255, 0, 0));
-
+	count = 0;
     };
 
 
     processing.draw = function() {
-	var frame = controller.frame();
-	
-	if(frame.hands.length == 0) {
-	    return;
-	}
-	// console.log(frame.hands.length);
-
-	frame.gestures.forEach(function(gesture){
-	    if (gesture.type == "keyTap") {
-		if (!isDrawing) { // start point
-		    points = [];
-		} else {          // end point
-		    console.log(points);
-
-		    // output
-		    var o = document.getElementById("output");
-		    o.innerHTML = JSON.stringify(points);
-		}
-
-	    	isDrawing = !isDrawing;
-	    	console.log("key tapped!!!");
-	    }
-	});
-
-	if (!isDrawing) {
+	drawPoint(points[count++]);
+	if(points.length <= count){
+	    count = 0;
 	    processing.background(0);
 	}
+    };
 
-	var hand = frame.hands[0];
-	var index = hand.indexFinger;
-	var x = index.tipPosition[0];
-	var y = index.tipPosition[1];
-	var z = index.tipPosition[2];
-	var point = {"x": x, "y": y, "z": z};
-	points.push(point);
-	// console.log("x: " + x, ", y: ", + y + ", z: " + z);
-
+    function drawPoint(point){
 	processing.pushMatrix();
-
-	processing.translate(processing.width/2, processing.height, 0);
-	processing.translate(x, -y, z);
-
+	processing.translate(point.x, -point.y, point.z);
 	processing.sphere(12);
 	processing.popMatrix();
-
-	// console.log(points);
-	// console.log(points.length);
-    };
+    }
 }
-
 
 var canvas = document.getElementById("sketch");
 // attaching the sketchProc function to the canvas
