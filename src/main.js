6be1d1c8ec6a59d0d4651b6e0460495d48e029db
asdmay <wrbmc.mc.pr@gmail.com>
2015-11-16 01:51:13 +0900

    外部ファイルにデータを置いて、類似度を測ることができた

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ var Sketch = require('./sketch.js');
 var showColor = '#87ceeb';
 var drawColor = '#000080';
 var sketch = new Sketch('sketch');
-var Data = require('./samples.json');
+var samples = require('./samples.json');
 
-console.log(Data);
+
 
 
 var points = [];
