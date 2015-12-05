36aab79fe513e3f62629730aa20393d44e3b4a45
asdmay <wrbmc.mc.pr@gmail.com>
2015-12-05 16:48:30 +0900

    検索結果にサムネイルが出せるようになった

--- src/main.js
+++ src/main.js
@@ -184,6 +184,10 @@ function searchData(){
 	    //console.log(item.score);
 	    $('#output').append(item.name).append('<br />');
 	    $('#output').append(item.score).append('<br />').append('<br />');
+	     // for instance
+	    var imgPath = './img/' + item.name + '.png';
+	    var img = '<img src="' + imgPath + '">';
+	    $('#output').append(img).append('<br />');
 	});
 	    console.log('search end');
 	// });
