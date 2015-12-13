30290daf1815877c78566fb513fa7fbedb4e4449
otknoy <otknoy@gmail.com>
2015-12-14 01:02:16 +0900

    Draw points using canvas

--- src/main.js
+++ src/main.js
@@ -251,6 +251,45 @@ function recordFinger(){
     isRecording = !isRecording;
 }
 
+function drawData(id, points) {
+    var cs  = document.getElementById(id);
+    var ctx = cs.getContext('2d');
+    var w = cs.width;
+    var h = cs.height;
+
+    // processing の map 関数と同じやつ
+    function mapValue(value, start1, stop1, start2, stop2) {
+	var ratio = (value - start1) / (stop1 - start1);
+	return start2 + ratio * (stop2 - start2);
+    }
+
+    // 時系列データ points の i 番目の点を表示するためのカウンタ
+    var i = 0;
+
+    function render() {
+	var p = points[i];
+	var x = mapValue(p.x, -256, 256, 0, w);
+	var y = mapValue(p.y, -256, 256, h, 0);
+
+        ctx.clearRect(0, 0, w, h);
+
+	ctx.strokeStyle = '#6DD900';
+        ctx.beginPath();
+	ctx.arc(x, y, 5, 0, Math.PI*2, false);
+	ctx.stroke();
+	// console.log('draw point');
+	// console.log(x);
+	// console.log(y);
+
+	i++;
+	if (points.length <= i) {
+	    i = 0;
+	}
+    }
+
+    setInterval(render, 1000/60.0);
+}
+
 function searchData(){
     if(isRecording){
 	isRecording = false;
@@ -270,6 +309,17 @@ function searchData(){
 		    append(item.score)
 	    ).trigger('create').append('<hr>');
 	    $('.view').show(img);
+
+	    // 動的にキャンバスを作成
+	    var $canvas = $('<canvas>').attr({
+		id: 'canvas-' + item.name,
+		width: 160,
+		height: 160
+	    });
+	    // 任意の位置にキャンバスを追加
+	    $("#output").append($canvas);
+	    // 指定した id のキャンバスに points を描く
+	    drawData('canvas-' + item.name, item.points);
 	});
 	console.log('search end');
 
