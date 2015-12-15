b67a8fbd037f50e32041de1eca1f2daf67662e25
asdmay <wrbmc.mc.pr@gmail.com>
2015-12-15 19:40:25 +0900

    sin波のデータを入れた

---
+++ src/test/data_sin.js
@@ -0,0 +1,22 @@
+var x;//x座標
+var y;//y座標
+var A = 100;//振幅
+var p = 0.0;//初期位相
+var w = 1.0;//角周波数
+var t_end = 275;
+var speed =24.0;//high//slow6.0//normal12//アニメーションのスピード
+
+    for(var t = -275; t < t_end; t += speed){
+	var d = [];
+	var rad = t * (Math.PI / 180);
+	x = t;
+	y = A*(Math.sin(w*(rad) + p))+275;
+	d.push({
+	    "x": x,
+	    "y": y,
+	    "z": 0
+	});
+	console.log(d);
+//	console.log(y);
+    }
+
