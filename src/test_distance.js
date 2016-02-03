d8531d57ac2b2e58729213f6b006b6feb44b0d0f
otknoy <otknoy@gmail.com>
2016-02-04 01:37:38 +0900

    distance module test

---
+++ src/test_distance.js
@@ -0,0 +1,22 @@
+var Distance = require('./distance.js');
+
+// test
+var samples = require('./test/data.json');
+var query = samples[0];
+
+var ts1 = query.points;
+console.log(ts1.length);
+
+for (var i = 0; i < samples.length; i++) {
+    var ts2 = samples[i].points;
+
+    // temporal
+    var tdist = Distance.temporalDistance(ts1, ts2);
+
+    // spatial
+    var sdist = Distance.spatialDistance(ts1, ts2);
+
+    console.log("temporal distance: " + tdist);
+    console.log("spatial distance: {x: " + sdist.x + ", y: " + sdist.y + "}");
+}
+
