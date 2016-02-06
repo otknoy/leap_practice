c3794a1f12f45578771adc3c821f9f8f26f7aecd
otknoy <otknoy@gmail.com>
2016-02-06 16:54:40 +0900

    Output csv

--- src/test_distance.js
+++ src/test_distance.js
@@ -6,16 +6,16 @@ var query = samples[0];
 
 var ts1 = query.points;
 
+console.log("ts1_name,ts2_name,temporal_dist,spatial_dist");
+
 for (var i = 0; i < samples.length; i++) {
     var ts2 = samples[i].points;
 
-    // temporal
+    // temporal distance
     var tdist = Distance.temporalDistance(ts1, ts2);
 
-    // spatial
+    // spatial distance
     var sdist = Distance.spatialDistance(ts1, ts2);
 
-    console.log("temporal distance: " + tdist);
-    // console.log("spatial distance: {x: " + sdist.x + ", y: " + sdist.y + "}");
-    console.log("spatial distance: " + sdist.x * sdist.y);
+    console.log(query.name + "," + samples[i].name + "," + tdist + "," + sdist.x*sdist.y);
 }
