4a8d473086b257f342f07f67969553abeaca34f4
otknoy <otknoy@gmail.com>
2016-02-06 17:09:41 +0900

    Fix output format

--- src/test_distance.js
+++ src/test_distance.js
@@ -6,7 +6,7 @@ var query = samples[0];
 
 var ts1 = query.points;
 
-console.log("ts1_name,ts2_name,temporal_dist,spatial_dist");
+console.log("ts1_name,ts2_name,score,score_s,score_t");
 
 for (var i = 0; i < samples.length; i++) {
     var ts2 = samples[i].points;
@@ -17,5 +17,9 @@ for (var i = 0; i < samples.length; i++) {
     // spatial distance
     var sdist = Distance.spatialDistance(ts1, ts2);
 
-    console.log(query.name + "," + samples[i].name + "," + tdist + "," + sdist.x*sdist.y);
+    var score = tdist * sdist.x * sdist.y;
+    var score_s = sdist.x * sdist.y;
+    var score_t = tdist;
+
+    console.log(query.name + "," + samples[i].name + "," + score + "," + score_s + "," + score_t);
 }
